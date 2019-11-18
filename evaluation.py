539c0e10938e424e275bde62162ee82f0d2f3881
Eileen Cho <echo@10-19-2-232.dynapool.wireless.nyu.edu>
2019-11-17 20:32:43 -0500

    evaluation script

---
+++ evaluation.py
@@ -0,0 +1,110 @@
+#Evaluation script
+
+import pickle as pkl
+import os
+import pandas as pd
+import random
+from generate_dataloaders import *
+
+
+def load_model_info(model_folder,model_type):
+    path = os.getcwd()
+    model_dir = path + '/models/' + model_folder
+
+    opts = torch.load(model_dir+'opts')
+    model = model_type(opts['vocab_size'], opts['emb_dim'])
+    model.load_state_dict(torch.load(model_dir+'model_dict.pt',map_location=lambda storage, loc: storage))
+    centroids = torch.load(model_dir+'centroids',map_location=lambda storage, loc: storage)
+    
+    return model, opts, centroids
+
+def get_predictions(model, centroids, val_loader,criterion,current_device):
+    model.eval()
+    token_list = []
+    cluster_assignment_list = []
+    flagged_index_list = []
+    original_label = []
+    for i, (tokens, labels, flagged_indices) in enumerate(val_loader):
+            tokens = tokens.to(current_device)
+            labels = labels.to(current_device)
+            flagged_indices = flagged_indices.to(current_device)
+            
+            # forward pass and compute loss
+            sentence_embed = model(tokens,flagged_indices)
+            cluster_loss, cluster_assignments = criterion(sentence_embed, centroids)
+            
+            # store in list
+            token_list+=tokens.tolist()
+            flagged_index_list+=flagged_indices.tolist()
+            cluster_assignment_list+=cluster_assignments.tolist()
+            original_label+=labels.tolist()
+            
+    return token_list, flagged_index_list, cluster_assignment_list, original_label
+
+
+def decode_predictions(token_list,index_list,cluster_assignment_list,dictionary,original_label):
+    decoded_tokens = [' '.join(dictionary.decode_idx_seq(sent)) for sent in token_list]
+    reviews = [decoded for decoded in decoded_tokens]
+    flagged_words = [r.split()[i] for (r,i) in zip(reviews,index_list)]
+    reviews = [review.split('<pad>')[0] for review in reviews]
+    df_pred = pd.DataFrame({'review':reviews,'index':index_list,'flagged_word':flagged_words,\
+                            'assignment':cluster_assignment_list,'original':original_label})
+    
+    pred_1 = df_pred[df_pred.assignment==1]
+    pred_0 = df_pred[df_pred.assignment==0]
+    
+    pred_1_manual_TP = len(pred_1[pred_1.original == 1]) / pred_1.shape[0]
+    pred_0_manual_TP = len(pred_0[pred_0.original == 1]) / pred_0.shape[0]
+    
+    if pred_1_manual_TP >= pred_0_manual_TP:
+        TP_cluster = pred_1
+        FP_cluster = pred_0
+    else:
+        TP_cluster = pred_0
+        FP_cluster = pred_1
+        TP_cluster.assignment =0
+        FP_cluster.assignment = 1
+
+    return TP_cluster, FP_cluster
+
+
+def performance_analysis(TP_cluster,FP_cluster):
+    TP_rate = len(TP_cluster[TP_cluster.original==1]) / TP_cluster.shape[0]
+    FP_rate = len(TP_cluster[TP_cluster.original==0]) / TP_cluster.shape[0]
+    FN_rate = len(TP_cluster[TP_cluster.original==1]) / TP_cluster.shape[0]
+    TN_rate = len(TP_cluster[TP_cluster.original==0]) / TP_cluster.shape[0]
+    
+    accuracy = (TP_rate + TN_rate) / (TP_rate + FP_rate + FN_rate + TN_rate)
+    precision = TP_rate / (TP_rate + FP_rate)
+    recall = TP_rate / (TP_rate + FN_rate)
+    f1_score = (2 * precision * recall) / (precision + recall)
+    
+    print("TP_rate:",TP_rate)
+    print("FP_rate:",FP_rate)
+    print("FN_rate:",FN_rate)
+    print("TN_rate:",TN_rate)
+    print("\n")
+    
+    print("Accuracy:",accuracy)
+    print("Precision:",precision)
+    print("Recall:",recall)
+    print("F1 score:",f1_score)
+
+
+def main(model, centroids, val_loader, criterion, data_dir, current_device):
+	#print(criterion)
+	token_list, index_list, cluster_assignment_list, original_label = get_predictions(model, centroids, val_loader, criterion, current_device)
+	print(f"Total examples in val loader: {len(index_list)}")
+	print(f"Assigned to cluster 1: {sum(cluster_assignment_list)}")
+	dictionary = pkl.load(open(data_dir+'dictionary.p','rb'))
+	pd.set_option('max_colwidth',0)
+	TP_cluster, FP_cluster = decode_predictions(token_list,index_list,cluster_assignment_list,dictionary,original_label)
+	performance_analysis(TP_cluster,FP_cluster)
+
+	return TP_cluster, FP_cluster
+
+
+
+
+
+
